1229252668722c73e2cf6a604f44e3ff38540aee
Josue Israel Amaya Torres <90207534+Josue616@users.noreply.github.com>
2024-09-09 12:42:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 import google.generativeai as genai
 
 app = Flask(__name__)
+CORS(app)
 
 genai.configure(api_key=os.environ["API_KEY"])
 
